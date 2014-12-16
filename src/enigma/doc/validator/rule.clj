be5e513614569fce45ee60384d33b93e22b6c8a5
Faris Nasution <nasution.faris@gmail.com>
2014-12-16 19:12:12 +0700

    Add fn to use rule we define

--- src/enigma/doc/validator/rule.clj
+++ src/enigma/doc/validator/rule.clj
@@ -27,3 +27,28 @@
        (alter-meta! (var ~name)
                     assoc
                     :doc ~docstring))))
+
+(defn- go-validate?
+  [v pass-nill?]
+  (or (not (nil? v))
+      (and (nil? v) (false? pass-nill?))))
+
+(defn- construct-rule
+  [r]
+  (into (-> r meta :settings :using) [r]))
+
+(defn- validate-rule
+  [r v]
+  (let [{:keys [message pass-nill?]} (-> r meta :settings)]
+    (when (go-validate? v pass-nill?)
+      (when-not (true? (r v))
+        (message v)))))
+
+(defn -validate-rules
+  [r v]
+  (let [{:keys [message pass-nill?]} (-> r meta :settings)
+        rules (construct-rule r)]
+    (->> rules
+         (map #(validate-rule % v))
+         (filter #(not (nil? %)))
+         first)))
