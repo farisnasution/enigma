6081a5745101615d0f0459b8ee1cdeaacf4039dd
Faris Nasution <nasution.faris@gmail.com>
2014-12-16 08:52:14 +0700

    Initial commit

---
+++ src/enigma/doc/validator/rule.clj
@@ -0,0 +1,27 @@
+(ns enigma.doc.validator.rule)
+
+(def -default-settings
+  {:type :rule
+   :settings {:message (fn [v] (str "Error! Value is: " v))
+              :pass-nill? true
+              :using []}})
+
+(defmacro defrule
+  [name & args]
+  (let [[docstring args] (if (-> args first string?)
+                           [(first args) (next args)]
+                           [nil args])
+        [settings args] (if (-> args first map?)
+                          [(update-in -default-settings
+                                      [:settings]
+                                      #(into % (first args))) (next args)]
+                          [-default-settings args])
+        [args body] [(first args) (next args)]]
+    `(do
+       (def ~name
+         (with-meta
+           (fn [~@args] ~@body)
+           ~settings))
+       (alter-meta! (var ~name)
+                    assoc
+                    :doc ~docstring))))
