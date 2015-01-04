27fd7322fb7d0713934972f4828bbb70c6007c49
Faris Nasution <nasution.faris@gmail.com>
2015-01-04 12:39:27 +0700

    Added dependency: [liberator "0.12.2"] and [com.novemberain/monger "2.0.1"]

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.12.2"]
+                 [com.novemberain/monger "2.0.1"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :plugins [[speclj "3.1.0"]]
   :test-paths ["spec"]
