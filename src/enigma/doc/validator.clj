e5cd9115ea08d9409c9adfcf13ba8d6c5f55bd84
Faris Nasution <nasution.faris@gmail.com>
2015-01-04 12:38:00 +0700

    Fix error on using private fn on macro. [validate] and [-validate-validator] now becomes multi-method

--- src/enigma/doc/validator.clj
+++ src/enigma/doc/validator.clj
@@ -1,9 +1,16 @@
 (ns enigma.doc.validator
   (:use [enigma.doc.validator.rule :only [-validate-rules]]))
 
-(defn- validate-validator
+(defmulti -validate-validator
+  (fn [_ value] (type value)))
+
+(defmethod -validate-validator clojure.lang.PersistentArrayMap
   [validator value]
   (validator value))
+
+(defmethod -validate-validator clojure.lang.PersistentVector
+  [validator value]
+  (map validator value))
 
 (defmulti validate
   (fn [rv _] (-> rv meta :type)))
@@ -14,7 +21,7 @@
 
 (defmethod validate :validator
   [validator value]
-  (validate-validator validator value))
+  (-validate-validator validator value))
 
 (defmethod validate :default
   [_ value]
@@ -24,15 +31,15 @@
   {:type :validator
    :settings {:strict? true}})
 
-(defn- validation-fn
+(defn -validation-fn
   [map-settings map-value k]
-  (let [validation-fn (get map-settings k)
+  (let [-validation-fn (get map-settings k)
         value (get map-value k)
-        result (validate validation-fn value)]
+        result (validate -validation-fn value)]
     (when-not (nil? result)
       {k result})))
 
-(defn- strict-validation
+(defn -strict-validation
   [map-settings map-value]
   (let [key-settings (set (keys map-settings))
         key-value (keys map-value)]
@@ -55,21 +62,23 @@
          (with-meta
            (fn [value#]
              (if-not (map? value#)
-               {:self-validation-error (str "Value is not a map: " value#)}
+               {:self-validation-error (str "Value is not a Map: " value#)}
                (let [map-rules# (hash-map ~@args)
                      ks# (keys map-rules#)
-                     val-fn# (partial validation-fn map-rules# value#)
+                     val-fn# (partial -validation-fn map-rules# value#)
                      result# (->> ks#
                                   (map #(val-fn# %))
                                   (filter #(not (nil? %)))
-                                  (reduce #(into %1 %2) {}))
+                                  (apply merge))
                      strict?# (-> ~settings :settings :strict? true?)]
                  (if-not strict?#
                    result#
-                   (let [strict-result# (strict-validation map-rules# value#)
-                         final-result# (assoc result#
-                                         :strict-error
-                                         strict-result#)]
+                   (let [strict-result# (-strict-validation map-rules# value#)
+                         final-result# (if (empty? strict-result#)
+                                         result#
+                                         (assoc result#
+                                           :strict-error
+                                           strict-result#))]
                      final-result#)))))
            (assoc ~settings :validator (hash-map ~@args))))
        (alter-meta! (var ~name)
