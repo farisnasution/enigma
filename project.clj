0067dba401fc2e0b1044071170d95efc64162f67
Faris Nasution <nasution.faris@gmail.com>
2015-01-06 08:42:52 +0700

    Add [buddy "0.2.3"]

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liberator "0.12.2"]
-                 [com.novemberain/monger "2.0.1"]]
+                 [com.novemberain/monger "2.0.1"]
+                 [buddy "0.2.3"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :plugins [[speclj "3.1.0"]]
   :test-paths ["spec"]
