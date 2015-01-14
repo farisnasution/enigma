f91c3c928e7f8a67efa4acf0fa15cee09ed4a64a
Faris Nasution <nasution.faris@gmail.com>
2015-01-14 18:22:00 +0700

    Add predefined rules and field.

---
+++ src/enigma/doc/field.clj
@@ -0,0 +1,129 @@
+(ns enigma.doc.field
+  (:require [enigma.doc.rule :as r]
+            [enigma.doc.validator.field :as vf])
+  (:use enigma.doc.validator.core))
+
+(defn- extract-required
+  [rule value]
+  (if-not (nil? value)
+    (not value)
+    (not (-> rule :settings :pass-nill?))))
+
+(defmacro extract-settings
+  [rule value ks]
+  `(if-not (nil? ~value)
+     ~value
+     (-> ~rule :settings ~@ks)))
+
+(vf/deffield string-field
+  :required r/required
+  :str-only r/str-only
+  :regex r/regex
+  :min-length r/min-length
+  :max-length r/max-length
+  [{:keys [required max-length min-length regex]}
+   {:keys [required? max-length? min-length? regex-config]}]
+  (let [required? (extract-required required required?)
+        min-length? (extract-settings min-length min-length? [:threshold])
+        max-length? (extract-settings max-length max-length? [:threshold])
+        regex-config (extract-settings regex regex-config [:regex-config])]
+   {:required (construct required {:pass-nill? required?})
+    :min-length (construct min-length {:threshold min-length?})
+    :max-length (construct max-length {:threshold max-length?})
+    :regex (construct regex {:regex-config regex-config})}))
+
+(vf/deffield int-field
+  :required r/required
+  :int-only r/int-only
+  :min-value r/min-value
+  :max-value r/max-value
+  [{:keys [required min-value max-value]}
+   {:keys [required? max-value? min-value?]}]
+  (let [required? (extract-required required required?)
+        min-value? (extract-settings min-value min-value? [:threshold])
+        max-value? (extract-settings max-value max-value? [:threshold])]
+    {:required (construct required {:pass-nill? required?})
+     :min-value (construct min-value {:threshold min-value?})
+     :max-value (construct max-value {:threshold max-value?})}))
+
+(vf/deffield float-field
+  :required r/required
+  :float-only r/float-only
+  :min-value r/min-value
+  :max-value r/max-value
+  [{:keys [required min-value max-value]}
+   {:keys [required? max-value? min-value?]}]
+  (let [required? (extract-required required required?)
+        min-value? (extract-settings min-value min-value? [:threshold])
+        max-value? (extract-settings max-value max-value? [:threshold])]
+    {:required (construct required {:pass-nill? required?})
+     :min-value (construct min-value {:threshold min-value?})
+     :max-value (construct max-value {:threshold max-value?})}))
+
+(vf/deffield vec-field
+  :required r/required
+  :vec-only r/vec-only
+  :cannot-empty r/cannot-empty
+  [{:keys [required cannot-empty]}
+   {:keys [required? can-empty?]}]
+  (let [required? (extract-required required required?)
+        can-empty? (extract-settings cannot-empty can-empty? [:can?])]
+    {:required (construct required {:pass-nill? required?})
+     :cannot-empty (construct cannot-empty can-empty?)}))
+
+(vf/deffield map-field
+  :required r/required
+  :map-only r/map-only
+  :cannot-empty r/cannot-empty
+  [{:keys [required cannot-empty]}
+   {:keys [required? can-empty?]}]
+  (let [required? (extract-required required required?)
+        can-empty? (extract-settings cannot-empty can-empty? [:can?])]
+    {:required (construct required {:pass-nill? required?})
+     :cannot-empty (construct cannot-empty can-empty?)}))
+
+(vf/deffield email-field
+  :required r/required
+  :str-only r/str-only
+  :email-only r/email-only
+  [{:keys [required]}
+   {:keys [required?]}]
+  (let [required? (extract-required required required?)]
+    {:required (construct required {:pass-nill? required?})}))
+
+(vf/deffield slug-field
+  :required r/required
+  :str-only r/str-only
+  :slug-only r/slug-only
+  [{:keys [required]}
+   {:keys [required?]}]
+  (let [required? (extract-required required required?)]
+    {:required (construct required {:pass-nill? required?})}))
+
+(vf/deffield date-field
+  :required r/required
+  :date-only r/date-only
+  [{:keys [required]}
+   {:keys [required?]}]
+  (let [required? (extract-required required required?)]
+    {:required (construct required {:pass-nill? required?})}))
+
+(vf/deffield bool-field
+  :required r/required
+  :bool-only r/bool-only
+  [{:keys [required]}
+   {:keys [required?]}]
+  (let [required? (extract-required required required?)]
+    {:required (construct required {:pass-nill? required?})}))
+
+(vf/deffield oid-field
+  :required r/required
+  :oid-only r/oid-only
+  [{:keys [required]}
+   {:keys [required?]}]
+  (let [required? (extract-required required required?)]
+    {:required (construct required {:pass-nill? required?})}))
+
+;; yang bisa di extend
+;; mapper
+;; validator
---
+++ src/enigma/doc/rule.clj
@@ -0,0 +1,163 @@
+(ns enigma.doc.rule
+  (:use enigma.doc.validator.core)
+  (:require [enigma.doc.validator.rule :as vr]))
+
+(vr/defrule required
+  {:message (fn [settings value] "Value cannot be nil.")
+   :pass-nill? false}
+  [_ value]
+  (not (nil? value)))
+
+(vr/defrule str-only
+  {:message (fn [_ value]
+              (str "Value should be string: " value))}
+  [_ value]
+  (string? value))
+
+(vr/defrule min-length
+  {:threshold -1
+   :message (fn [{:keys [threshold]} value]
+              (str "Value's length is below or equal to threshold. "
+                   "Value: " value "."
+                   "Threshold: " threshold "."))}
+  [{:keys [threshold]} value]
+  (or (= threshold -1) (> (count value) threshold)))
+
+(vr/defrule max-length
+  {:threshold -1
+   :message (fn [{:keys [threshold]} value]
+              (str "Value's length is above or equal to threshold. "
+                   "Value: " value "."
+                   "Threshold: " threshold "."))}
+  [{:keys [threshold]} value]
+  (or (= threshold -1) (< (count value) threshold)))
+
+(vr/defrule int-only
+  {:message (fn [_ value]
+              (str "Value should be an int: " value))}
+  [_ value]
+  (integer? value))
+
+(vr/defrule float-only
+  {:message (fn [_ value]
+              (str "Value should be a float: " value))}
+  [_ value]
+  (float? value))
+
+(vr/defrule min-value
+  {:threshold -1
+   :message (fn [{:keys [threshold]} value]
+              (str "Value is below or equal to threshold. "
+                   "Value: " value "."
+                   "Threshold: " threshold "."))}
+  [{:keys [threshold]} value]
+  (or (= threshold -1) (> value threshold)))
+
+(vr/defrule max-value
+  {:threshold -1
+   :message (fn [{:keys [threshold]} value]
+              (str "Value's length is above or equal to threshold. "
+                   "Value: " value "."
+                   "Threshold: " threshold "."))}
+  [{:keys [threshold]} value]
+  (or (= threshold -1) (< value threshold)))
+
+(vr/defrule vec-only
+  {:message (fn [_ value]
+              (str "Value is not a vector: " value "."))}
+  [_ value]
+  (vector? value))
+
+(vr/defrule map-only
+  {:message (fn [_ value]
+              (str "Value is not a map: " value "."))}
+  [_ value]
+  (map? value))
+
+(vr/defrule set-only
+  {:message (fn [_ value]
+              (str "Value is not a set: " value "."))}
+  [_ value]
+  (set? value))
+
+(vr/defrule cannot-empty
+  {:can? false
+   :message (fn [_ value]
+              (str "Value is empty: " value "."))}
+  [{:keys [can?]} value]
+  (or (true? can?) (boolean (seq val))))
+
+(vr/defrule pos-only
+  {:message (fn [_ value]
+              (str "Value is not positive: " value "."))}
+  [_ value]
+  (pos? value))
+
+(vr/defrule neg-only
+  {:message (fn [_ value]
+              (str "Value is not negative: " value "."))}
+  [_ value]
+  (neg? value))
+
+(vr/defrule date-only
+  {:message (fn [_ value]
+              (str "Value is not a date. "
+                   "Value: " value ". "
+                   "Type: " (type value)))}
+  [_ value]
+  (instance? java.util.Date value))
+
+(vr/defrule regex
+  {:regex-config #"."
+   :message (fn [{:keys [regex-config]} value]
+              (str "Value didn't match with the regex given. "
+                   "Value: " value ". "
+                   "Regex: " regex-config "."))}
+  [{:keys [regex-config]} value]
+  (and (string? value) (boolean (re-matches regex-config value))))
+
+(vr/defrule email-only
+  {:message (fn [_ value]
+              (str "Value is not email-compatible: " value "."))}
+  [_ value]
+  (let [regex-config #"[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.+_-]+\.[a-zA-Z]{2,4}"]
+    (-> regex
+        (construct {:regex-config regex-config})
+        (validate value)
+        boolean)))
+
+(vr/defrule bool-only
+  {:message (fn [_ value]
+              (str "Value is not a boolean: " value "."))}
+  [_ value]
+  (or (true? value) (false? value)))
+
+(vr/defrule slug-only
+  {:message (fn [_ value]
+              (str "Value is not a slug-compatible: " value "."))}
+  [_ value]
+  (let [regex-config #"^[a-z0-9]+(?:-[a-z0-9]+)*$"]
+    (-> regex
+        (construct {:regex-config regex-config})
+        (validate value)
+        boolean)))
+
+(vr/defrule oid-only
+  {:message (fn [_ value]
+              (str "Value is not an instance of ObjectId. "
+                   "Value: " value ". "
+                   "Type: " (if-not (nil? value)
+                              (type value)
+                              "nil") "."))}
+  [_ value]
+  (instance? org.bson.types.ObjectId (type value)))
+
+(vr/defrule every
+  {:message (fn [_ v] "Value doesn't satisfy the given :every-fn.")}
+  [{:keys [every-fn]} value]
+  (every? every-fn value))
+
+(vr/defrule any
+  {:message (fn [_ v] "Value doesn't satisfy the given :any-fn.")}
+  [{:keys [any-fn]} value]
+  (not (not-any? any-fn value)))
