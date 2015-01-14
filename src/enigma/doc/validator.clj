310f44d15dac2ed104bf68722895c78a167fd882
Faris Nasution <nasution.faris@gmail.com>
2015-01-14 18:21:01 +0700

    Validator now uses ValidationFn protocol.

--- src/enigma/doc/validator.clj
+++ src/enigma/doc/validator.clj
@@ -1,51 +1,59 @@
 (ns enigma.doc.validator
-  (:use [enigma.doc.validator.core :only [-validate-rules]]))
-
-(defmulti -validate-validator
-  (fn [_ value] (type value)))
-
-(defmethod -validate-validator clojure.lang.PersistentArrayMap
-  [validator value]
-  (validator value))
-
-(defmethod -validate-validator clojure.lang.PersistentVector
-  [validator value]
-  (map validator value))
-
-(defmulti validate
-  (fn [rv _] (-> rv meta :type)))
-
-(defmethod validate :rule
-  [rule value]
-  (-validate-rules rule value))
-
-(defmethod validate :validator
-  [validator value]
-  (-validate-validator validator value))
-
-(defmethod validate :default
-  [_ value]
-  {:fn-error "Fn is not a rule nor a validator."})
+  (:use enigma.doc.validator.core))
 
 (def -default-settings
-  {:type :validator
-   :settings {:strict? true}})
-
-(defn -validation-fn
-  [map-settings map-value k]
-  (let [-validation-fn (get map-settings k)
-        value (get map-value k)
-        result (validate -validation-fn value)]
-    (when-not (nil? result)
-      {k result})))
+  {:strict? true})
 
 (defn -strict-validation
-  [map-settings map-value]
-  (let [key-settings (set (keys map-settings))
-        key-value (keys map-value)]
+  [map-body map-data]
+  (let [key-settings (set (keys map-body))
+        key-value (keys map-data)]
     (vec (filter (fn [k]
                    (when (nil? (key-settings k)) k))
                  key-value))))
+
+(defn- validation-fn
+  [map-body map-data]
+  (fn [k]
+    (let [func (get map-body k)
+          data (get map-data k)
+          result (func data)]
+      (when-not (nil? result)
+        {k result}))))
+
+(defn- validate-with-validator
+  [{:keys [body settings]} data]
+  (if-not (map? data)
+    {:self-validation-error (format "Value is not a Map: %s" data)}
+    (let [strict? (:strict? settings)
+          ks (keys body)
+          v-fn (validation-fn body data)
+          result (->> ks
+                      (map v-fn)
+                      (filter #(not (nil? %)))
+                      (apply merge))]
+      (if-not (true? strict?)
+        result
+        (let [strict-result (-strict-validation body data)
+              final-result (if (empty? strict-result)
+                             result
+                             (assoc result :strict-error strict-result))]
+          final-result)))))
+
+(defn- construct-validator
+  [this opts]
+  (assoc this :settings opts))
+
+(defrecord Validator [body settings]
+  ValidationFn
+  (validate [this data]
+    (validate-with-validator this data))
+  (construct [this data]
+    (construct-validator this data)))
+
+(defn validator?
+  [v]
+  (instance? enigma.doc.validator.Validator v))
 
 (defmacro defvalidator
   [name & args]
@@ -53,34 +61,22 @@
                            [(first args) (next args)]
                            [nil args])
         [settings args] (if (-> args first map?)
-                          [(update-in -default-settings
-                                      [:settings]
-                                      #(into % (first args))) (next args)]
-                          [-default-settings args])]
+                          [(first args) (next args)]
+                          [-default-settings args])
+        [parent args] (if (-> args first validator?)
+                        [(first args) (next args)]
+                        [{} args])]
     `(do
        (def ~name
-         (with-meta
-           (fn [value#]
-             (if-not (map? value#)
-               {:self-validation-error (str "Value is not a Map: " value#)}
-               (let [map-rules# (hash-map ~@args)
-                     ks# (keys map-rules#)
-                     val-fn# (partial -validation-fn map-rules# value#)
-                     result# (->> ks#
-                                  (map #(val-fn# %))
-                                  (filter #(not (nil? %)))
-                                  (apply merge))
-                     strict?# (-> ~settings :settings :strict? true?)]
-                 (if-not strict?#
-                   result#
-                   (let [strict-result# (-strict-validation map-rules# value#)
-                         final-result# (if (empty? strict-result#)
-                                         result#
-                                         (assoc result#
-                                           :strict-error
-                                           strict-result#))]
-                     final-result#)))))
-           (assoc ~settings :validator (hash-map ~@args))))
-       (alter-meta! (var ~name)
-                    assoc
-                    :doc ~docstring))))
+         (let [parent-body# (:body ~parent)
+               parent-settings# (:settings ~parent)
+               body# (if-not (nil? parent-body#)
+                       (into parent-body# (hash-map ~@args))
+                       (hash-map ~@args))
+               settings# (if-not (nil? parent-settings#)
+                           (into parent-settings# ~settings)
+                           ~settings)]
+           (->Validator body# settings#)))
+       (alter-meta (var ~name)
+                   assoc
+                   :doc ~docstring))))
