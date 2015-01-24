99c7bcc196dab1b66c4c4912b444655dc05afddb
Faris Nasution <nasution.faris@gmail.com>
2015-01-24 08:55:12 +0700

    Now uses defrecord.

--- src/enigma/doc/mapper.clj
+++ src/enigma/doc/mapper.clj
@@ -1,50 +1,5 @@
 (ns enigma.doc.mapper)
 
-;; (def -default-settings {:type :mapper
-;;                         :settings {}})
-
-;; (defn- process?
-;;   [map-settings map-value k]
-;;   (let [s (get map-settings k)
-;;         result (boolean (or (contains? map-value k)
-;;                             (and (not (contains? map-value k))
-;;                                  (true? s))))]
-;;     result))
-
-;; (defn- go-process
-;;   [map-fn map-value k]
-;;   (let [f (get map-fn k)
-;;         v (get map-value k)
-;;         result {k (f map-value v)}]
-;;     result))
-
-;; (defmacro defmapper
-;;   [name & args]
-;;   (let [[docstring args] (if (-> args first string?)
-;;                            [(first args) (next args)]
-;;                            [nil args])
-;;         [settings args] (if (-> args first map?)
-;;                           [(assoc -default-settings :settings (first args))
-;;                            (next args)]
-;;                           [-default-settings args])]
-;;     `(do
-;;        (def ~name
-;;          (with-meta
-;;            (fn [value#]
-;;              (let [map-mapper# (hash-map ~@args)
-;;                    process?# (partial process? ~settings value#)
-;;                    go-process# (partial go-process map-mapper# value#)
-;;                    ks# (keys map-mapper#)
-;;                    result# (->> ks#
-;;                                 (filter #(process?# %))
-;;                                 (map #(go-process# %))
-;;                                 (apply merge))]
-;;                result#))
-;;            (assoc ~settings :mapper (hash-map ~@args))))
-;;        (alter-meta! (var ~name)
-;;                     assoc
-;;                     :doc ~docstring))))
-
 (defn- process?
   [settings data]
   (fn [k]
@@ -80,7 +35,7 @@
 
 (defn mapper?
   [m]
-  (instance? enigma.doc.mapper.Mapper m ))
+  (instance? enigma.doc.mapper.Mapper m))
 
 (defmacro defmapper
   [name & args]
@@ -89,20 +44,21 @@
                            [nil args])
         [settings args] (if (-> args first map?)
                           [(first args) (next args)]
-                          [{} args])
-        [parent args] (if (-> args first mapper?)
-                        [(first args) (next args)]
-                        [{} args])]
+                          [{} args])]
     `(do
        (def ~name
-         (let [parent-body# (:body ~parent)
-               parent-settings# (:settings ~parent)
+         (let [args# (list ~@args)
+               [parent# args#] (if (-> args# first mapper?)
+                                 [(first args#) (next args#)]
+                                 [{} args#])
+               {parent-body# :body parent-settings# :settings} parent#
                body# (if-not (nil? parent-body#)
-                       (into parent-body# (hash-map ~@args))
-                       (hash-map ~@args))
+                       (into parent-body# (apply hash-map args#))
+                       (apply hash-map args#))
                settings# (if-not (nil? parent-settings#)
                            (into parent-settings# ~settings)
-                           ~settings)]))
-       (alter-meta (var ~name)
-                   assoc
-                   :doc ~docstring))))
+                           ~settings)]
+           (->Mapper body# settings#)))
+       (alter-meta! (var ~name)
+                    assoc
+                    :doc ~docstring))))
