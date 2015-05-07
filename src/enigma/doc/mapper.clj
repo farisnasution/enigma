e8d8d90a8c6ec3f908ca6ffaa980bbb63323a641
Faris Nasution <nasution.faris@gmail.com>
2015-05-07 12:48:25 +0700

    Now [mapper] can accept any arg but will return nil except for a map.

--- src/enigma/doc/mapper.clj
+++ src/enigma/doc/mapper.clj
@@ -17,13 +17,16 @@
 
 (defn- go-process
   [{:keys [body settings]} data]
-  (let [ks (keys body)
-        process?-fn (process? settings data)
-        go-process-inner-fn (go-process-inner body data)]
-    (->> ks
-         (filter process?-fn)
-         (map go-process-inner-fn)
-         (apply merge))))
+  (when (map? data)
+    (if-not (empty? data)
+      (let [ks (keys body)
+            process?-fn (process? settings data)
+            go-process-inner-fn (go-process-inner body data)]
+        (->> ks
+             (filter process?-fn)
+             (map go-process-inner-fn)
+             (apply merge)))
+      {})))
 
 (defprotocol MappingFn
   (process [this data]))
