74c5e8d452c77cca5c10808418cfb7b29f1d8518
Faris Nasution <nasution.faris@gmail.com>
2015-05-09 11:51:40 +0700

    Removed.

--- src/enigma/app/base/validator.clj
+++
@@ -1,7 +0,0 @@
-(ns enigma.app.base.validator
-  (:require [enigma.doc.field :as f])
-  (:use [enigma.doc.validator :only [defvalidator]]))
-
-(defvalidator base-validator
-  :_id f/oid-field
-  :date-created f/date-field)
